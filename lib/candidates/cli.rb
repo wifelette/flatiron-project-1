8fc675b7ca2960d61bdeb3e7221e402cb474a931
wifelette <leahsilber@gmail.com>
2019-10-10 19:56:03 -0700

    Add inline docs and uniformly label TODOs

--- lib/candidates/cli.rb
+++ lib/candidates/cli.rb
@@ -11,7 +11,7 @@ module Candidates
   PASTEL = Pastel.new
   # TTY adds numerous formatting options for data+copy in the terminal
   PROMPT = TTY::Prompt.new
-  # This constant is used by the TTY (sub)gem tty-markdown, also for display stuff
+  # This constant is used by the TTY (sub)gem `tty-markdown`, also for display stuff
   # In this case I used it to overwrite default markdown styling to get some of my longer text blocks to look the way I wanted them to look.
   THEME = {
     em: [:magenta, :bold],
@@ -21,10 +21,12 @@ module Candidates
     list: :yellow,
     strong: [:yellow, :bold],
     table: :yellow,
-    quote: :yellow,
+    quote: :yellow
   }
+
   class Cli < Thor
     def help(*)
+      # help is a built-in method in Thor; this code is how I can insert text into its output, ahead of the built-in content
       puts
       help_file = File.expand_path('../../markdown/help.md', __dir__)
       parsed = TTY::Markdown.parse_file(help_file, theme: THEME)
@@ -33,9 +35,11 @@ module Candidates
       super
     end
 
+    # This next line is what tells the built-in `help` command what to display when describing the method
     desc "user USERNAME", "Gets info about a Github user"
     def user(username)
       candidate = Candidate.new(username)
+      # There are a lot of puts around; it's just the simplest way to add whitespace in the command line, which makes the tool easier to read/use
       puts
       say <<~WRAPPED
         #{PASTEL.on_magenta("Here's everything you need to know about #{username}:")}
@@ -72,7 +76,7 @@ module Candidates
         #{PASTEL.magenta.bold('THEIR ACTIVITY:')}
         
         #{PASTEL.magenta.bold('Joined GitHub:')} #{candidate.created}
-        TODO: 1. (X years and Y months ago), 2. Format the date pretty
+        # TODO: 1. (X years and Y months ago), 2. Format the date pretty
         #{PASTEL.magenta.bold('Org Membership:')} #{candidate.org_count}
         #{PASTEL.magenta.bold('Public Repos:')} #{candidate.repos}
         #{PASTEL.magenta.bold('Followers:')} #{candidate.followers}
@@ -96,25 +100,27 @@ module Candidates
       puts candidate.company
     end
 
-    # This makes it a private method that won't appear in the Thor help command
+    # This makes what comes after it a private method that won't appear in the Thor help command
     no_commands do 
       def orgs(username)
         HTTParty.get("https://api.github.com/users/#{username}/orgs").parsed_response
       end
 
       def format_username(username)
+        # This exists just because PASTEL makes the copy really hard to scan, and we use the colored Username a lot.
         "#{PASTEL.magenta.bold("#{username}")}"
       end
       
       def prompts(username)
         pretty_name = format_username(username)
         choices = [
-          { value: :userinfo,    name: "Look up #{pretty_name}'s general information" },
-          { value: :orgs,      name: "Tell me about the orgs #{pretty_name} belongs to" },
-          { value: :newuser, name: "Look up a different candidate" },
-          { value: :help,      name: "Remind me what the #{PASTEL.magenta.bold('`candidates`')} gem can do" },
-          { value: :exit,      name: "Exit the program" }
+          { value: :userinfo, name: "Look up #{pretty_name}'s general information" },
+          { value: :orgs,     name: "Tell me about the orgs #{pretty_name} belongs to" },
+          { value: :newuser,  name: "Look up a different candidate" },
+          { value: :help,     name: "Remind me what the #{PASTEL.magenta.bold('`candidates`')} gem can do" },
+          { value: :exit,     name: "Exit the program" }
         ]
+        # This format spits out the question, followed by (comma) the options the user can choose from
         PROMPT.select("How can I help you learn about #{pretty_name}?", choices)
       end
 
@@ -124,6 +130,7 @@ module Candidates
         puts
         puts "#{pretty_name} is a member of #{PASTEL.magenta.bold("#{candidate.org_count}")} organizations:"
         puts
+        # The next two lines display a numbered list of all the orgs the candidate belongs to
         orgs_hashes = orgs(candidate.username)
         candidate.org_names(orgs_hashes)
         puts
@@ -133,10 +140,10 @@ module Candidates
           puts "Happy to help. Fetching the data now..."
           puts
           progressbar = ProgressBar.create
-          # Right now this is just making a random progressbar that then disappear; gotta revisit later so it's more meaningful
+          # TODO: Right now this is just making a random progressbar that then disappear; gotta revisit later so it's more meaningful
           4.times { progressbar.increment; sleep 1 }
-          # Loop in here to return an array of the names of all the user's orgs
           ap orgs_hashes
+          # TODO: It would be fun later to make the orgs display in a table rather than a hash. Try `tty-table`
           puts
           puts "There you go! #{PASTEL.yellow('ProTip')}: Command + click on any of these URLs in most Terminals to go directly to the link."
           puts
@@ -149,10 +156,11 @@ module Candidates
     desc "wizard", "Interactive Wizard that asks the user for input and helps them with subsequent questions"
     def wizard
       puts
+      # This is fetching all the pretty text I want to show here from the .md file
       prompt_file = File.expand_path('../../markdown/prompt.md', __dir__)
-      parsed = TTY::Markdown.parse_file(prompt_file, theme: THEME)
-      puts parsed
+      puts TTY::Markdown.parse_file(prompt_file, theme: THEME)
       puts
+      # The query below is blank because I included the question itself in the previous markdown file. Doing it this way has the added bonus of effectively putsing another blank line I wanted.
       username = PROMPT.ask("")
       new_candidate = Candidate.new(username)
 
@@ -162,17 +170,22 @@ module Candidates
 
         case response
         when :userinfo
+          # If they choose option 1, call the `user` method to display all the pretty-formatted info for the user
           puts
           user(username)
         when :orgs
+          # If they choose option 2, send them into the org line of questioning: how many, details y/n, etc.
           org_detail(new_candidate)
         when :newuser
+          # If they choose option 3, get the name of a new candidate from the user and instantiate it
           username = PROMPT.ask("What's the Github username of this next candidate?")
           new_candidate = Candidate.new(username)
         when :help
+          # If they choose option 4, call the built-in `help` method to display a list of everything they can do
           puts
           help
         when :exit
+          # If they choose option 5, display a parting greeting and exit the program
           puts
           puts "#{PASTEL.magenta.bold('Goodbye then!')}"
           puts
